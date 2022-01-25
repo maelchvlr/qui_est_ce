f3c73bcb460210a985aa590bf9a1fdf623304986
projetprogl2 <vert.stylo@laposte.net>
2022-01-25 13:34:23 +0100

    lib dependencies test

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.databind.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("main launched");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+    }
+}
---
+++ src/OTF.java
@@ -0,0 +1,8 @@
+public class OTF {
+	
+	private String name;
+
+	public OTF(String name) {
+		this.name = name;
+	};
+}
